09604f486c8e90a3149592e4873ac12f76fda2e1
Alexandre Bertails <bertails@w3.org>
2011-12-29 17:27:22 -0500

    ~ POST /observation

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,82 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.format.Formats._
+import play.api.data.validation.Constraints._
+import play.api.data.format.Formatter
+import java.net.URI
+import java.util.UUID
+import akka.actor.{ActorRef, Actor, Scheduler, UntypedChannel, TypedActor}
+import org.w3.util._
+import org.w3.vs.model._
+import org.w3.vs.observer._
+import play.api.data.FormError
 
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
+  }
+  
+  implicit val urlFormat = new Formatter[URL] {
+    
+    override val format = Some("format.url", Nil)
+    
+    def bind(key: String, data: Map[String, String]) = {
+      stringFormat.bind(key, data).right.flatMap { s =>
+        scala.util.control.Exception.allCatch[URL]
+          .either(URL(s))
+          .left.map(e => Seq(FormError(key, "error.url", Nil)))
+      }
+    }
+    
+    def unbind(key: String, url: URL) = Map(key -> url.toString)
+    
+  }
+  
+  val validateForm = Form(
+    of(
+      "url" -> of[URL],
+      "distance" -> of[Int].verifying(min(0), max(10)),
+      "linkCheck" -> of[Boolean]
+    )
+  )
+  
+  def validateWithParams(
+      url: URL,
+      distance: Int,
+      linkCheck: Boolean) = {
+    
+    val strategy = EntryPointStrategy(
+      uuid=java.util.UUID.randomUUID(), 
+      name="demo strategy",
+      entrypoint=url,
+      distance=distance,
+      linkCheck=linkCheck,
+      filter=Filter(include=Everything, exclude=Nothing))
+    
+    val observerId: ObserverId = ObserverId()
+    
+    val observer = Observer.newObserver(observerId, strategy)
+    
+    observer.startExplorationPhase()
+    
+    val observerIdString: String = observerId.toString
+    
+    Created("").withHeaders(
+      "Location" -> new URI(observerIdString).toString,
+      "X-VS-ActionID" -> observerIdString)
+  }
+  
+  def validate() = Action { implicit request =>
+    validateForm.bindFromRequest.fold(
+      formWithErrors => { Logger.error(formWithErrors.errors.toString); BadRequest(formWithErrors.toString) },
+      v => validateWithParams(v._1, v._2, v._3)
+    )
+    
+    
+    
   }
   
 }
---
+++ public/javascripts/vs.js
@@ -0,0 +1,273 @@
+//todo 	
+//		- close/cancel validation
+//		- logs limit - clear only if necessary?
+//		- toggle autoscroll
+
+var VS = {
+	
+	initialize: function() {
+		VS.logs = $('#logs');
+		VS.logs.list = $('#logs ul');
+		VS.stats = $('#stats');
+		VS.stats.urls = $('#stats li.urls :last-child');
+		VS.stats.observations = $('#stats li.observations :last-child');
+		VS.stats.errors = $('#stats li.errors :last-child');
+		VS.stats.warnings = $('#stats li.warnings :last-child');
+		VS.form = $('#form form');
+		VS.atmosphereRequest = {transport: 'streaming'};
+		
+		VS.parseHash();
+		VS.formValidateAction();
+		
+		$('a.clear').click(function () {
+			VS.clearLogs();
+			return false;
+		});
+	},
+	
+	formValidateAction: function() {
+		var options = { 
+			success:	VS.subscribe,
+			error:		VS.xhrError
+		}; 
+		//VS.form.ajaxForm(options); 
+		VS.form.submit(function() { 
+			VS.clearLogs();
+			VS.clearStats();
+			$(this).ajaxSubmit(options);
+			return false; 
+		});
+	},
+	
+	formStopAction: function() {
+		
+	},
+	
+	subscribe: function(responseText, statusText, xhr) {
+		VS.log("<li class='status'>Starting crawl of " + $("input#url", VS.form).val() + " with a distance of " + $("input#distance", VS.form).val() + "</li>");
+		var loc = xhr.getResponseHeader("Location");
+		VS.setHash('!' + loc.substr(loc.search('/observation/[^/]+$')));
+		$.atmosphere.subscribe(
+			loc + "/stream",
+			VS.logResponse,
+			VS.atmosphereRequest
+		);
+	},
+	
+	xhrError: function(jqXHR, textStatus, errorThrown) {
+		console.log(jqXHR);
+		console.log(textStatus);
+		console.log(errorThrown);
+	},
+	
+	clearLogs: function() {
+		VS.logs.list.empty();
+	},
+	
+	clearStats: function() {
+		VS.stats.urls.text("0/0");
+		VS.stats.observations.text("0/0");
+		VS.stats.errors.text("0");
+		VS.stats.warnings.text("0");
+	},
+	
+	logResponse: function(response) {
+		if (response.state == "error") {
+			console.log("Error state response:");
+			console.log(response);
+			return;
+		}
+		if (response.status == "404") {
+			//console.log(response);
+			VS.log($('<li>', {
+				'class': 'error',
+				 html  : 'Invalid observationID: ' + VS.parseHeader(response.headers)["X-VS-ObservationID"]
+			}));
+			VS.setHash("");
+			return;
+		}
+		if (response.status != "200") {
+			VS.log($('<li>', {
+				'class': 'error',
+				 html  : 'Error code: ' + response.status
+			}));
+			return;
+		}
+		if (response.responseBody != "") {
+			try {
+				var messages = response.responseBody.match(/\[[^\[]+\]/g);
+				for (var i=0;i<messages.length;i++) {
+					var msg = $.parseJSON(messages[i]);
+					VS.logJson(msg);
+				}
+			} catch(e) {
+				console.log(e.toString() + " - " + response.responseBody);
+				VS.log(e.toString() + " - " + response.responseBody);
+			}
+		} else {
+			console.log("Empty response body:");
+			console.log(response);
+		}
+	},
+	
+	logJson: function(msg) {
+		var type = msg[0];
+		console.log(msg);
+		if (type == "GET") {
+			VS.incrementCrawled(1,msg[3]);
+			var ext = "";
+			if (parseInt(msg[3]) > 0)
+				ext = " (+" + msg[3] + ")";
+			VS.log($('<li>', {
+				'class': 'fetch get c' + msg[1],
+				 html  : '<span>GET</span> <span>(' + msg[1] + ')</span> <span>' + msg[2] + '</span> <span>' + ext + '</span>'
+			}));
+		} else if (type == "HEAD") {
+			VS.incrementCrawled(1,0);
+			VS.log($('<li>', {
+				'class': 'fetch head c' + msg[1],
+				 html  : '<span>HEAD</span> <span>(' + msg[1] + ')</span> <span>' + msg[2] + '</span>'
+			}));
+		} else if (type == "ERR") {
+			//VS.log(response.responseBody);
+			VS.log($('<li>', {
+				'class': 'fetch error',
+				 html  : '<span>ERROR</span> <span>(' + msg[1] + ')</span> <span>' + msg[2] + '</span>'
+				 //html  : 'ERR: '+msg[1]+' - '+msg[2]
+			}));
+		} else if (type == "OBS") {
+			VS.incrementObserved(1,0);
+			var errors = parseInt(msg[3]);
+			var warnings = parseInt(msg[4]);
+			VS.incrementErrors(errors);
+			VS.incrementWarnings(warnings);
+			var str = " &nbsp;»&nbsp; ";
+			if (errors == 0)
+				if (warnings == 0)
+					str += "<span class='valid'>valid</span>";
+				else
+					str += "<span class='warnings'>" + warnings + "</span> warning(s)";
+			else {
+				str += "<span class='errors'>" + errors + "</span> errors(s)";
+				if (warnings > 0)
+					str += " and <span class='warnings'>" + warnings + "</span> warning(s)";
+			}
+			VS.log($('<li>', {
+				'class': 'obs',
+				 html  : '<span>'+msg[2]+':</span> '+ msg[1] + str
+			}));
+		} else if (type == "OBS_ERR") {
+			VS.log(response.responseBody);
+		} else if (type == "OBS_NO") {
+			VS.log($('<li>', {
+				'class': 'obs none',
+				 html  : 'No observer available for '+ msg[1]
+			}));
+		} else if (type == "NB_EXP") {
+			VS.incrementCrawled(0,msg[1]);
+		} else if (type == "NB_OBS") {
+			VS.log("<li class='status'>Starting observation phase</li>");
+			VS.incrementObserved(0, msg[1]);
+		} else if (type == "OBS_FINISHED") {
+			$.atmosphere.unsubscribe();
+			VS.log("<li class='status'>Validation finished</li>");
+		} else if (type == "OBS_INITIAL") {
+			VS.incrementCrawled(0,msg[1]+msg[2]);
+			VS.incrementObserved(0,msg[3]+msg[4]);
+		} else {
+			console.log(msg);
+			VS.log(msg);
+		}
+	},
+	
+	log: function(message) {
+		console.log(message);
+		if (typeof message === "string")
+			message = $('<li>' + message + '</li>');
+		VS.logs.list.append(message);
+		VS.logs.scrollTop(VS.logs.list.height());
+	},
+	
+	parseHeader: function(headers) {
+		var arr = headers.split("\n");
+		var result = {};
+		for(var i=0;i<arr.length;i++) {
+			var h = arr[i].split(':');
+			result[h[0]] = $.trim(h[1]);
+		}
+		return result;
+	},
+	
+	incrementCrawled: function(crawledInc, totalInc) {
+		VS.incrementStats(crawledInc, totalInc, VS.stats.urls);
+	},
+	
+	incrementObserved: function(obsInc, totalInc) {
+		VS.incrementStats(obsInc, totalInc, VS.stats.observations);
+	},
+	
+	incrementStats: function(left, right, node) {
+		if (typeof node.text !== "function") {
+			console.log("ERROR: object passed to incrementStats method is not a jQuery object");
+			return;
+		}
+		var val = node.text().split("/");
+		node.text(
+			(parseInt(val[0]) + parseInt(left)) + "/" + (parseInt(val[1]) + parseInt(right))
+		);
+		
+		$('#progress').css('display', 'inline');
+		$('#progress').attr('value', parseInt(val[0]) + parseInt(left));
+		$('#progress').attr('max', parseInt(val[1]) + parseInt(right));
+	},
+	
+	incrementErrors: function(nb) {
+		VS.stats.errors.text(
+			parseInt(VS.stats.errors.text()) + nb
+		);
+	},
+	
+	incrementWarnings: function(nb) {
+		VS.stats.warnings.text(
+			parseInt(VS.stats.warnings.text()) + nb
+		);
+	},
+	
+	updateObservations: function() {
+		
+	},
+	
+	parseHash: function() {
+		var hash = window.location.hash;
+		//console.log(hash);
+		if (hash.indexOf('#!/observation/') == 0)
+			$.atmosphere.subscribe(
+				hash.substr(hash.search('/observation/[^/]+$')) + "/stream",
+				VS.logResponse,
+				VS.atmosphereRequest
+			);
+	},
+	
+	setHash: function(hash){
+		if (window.webkit419){
+			W3C.FakeForm = W3C.FakeForm || new Element('form', {'method': 'get'}).injectInside(document.body);
+			W3C.FakeForm.setProperty('action', '#' + hash).submit();
+		} else {
+			window.location.replace('#' + hash);
+		}
+	},
+	
+//	log: function (level, args) {
+//        if (window.console) {
+//            var logger = window.console[level];
+//            if (typeof logger == 'function') {
+//                logger.apply(window.console, args);
+//            }
+//        }
+//    }
+	
+};
+
+$(document).ready(function() {
+	VS.initialize();
+});
