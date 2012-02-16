6e9b0c5e97138d7ce3d1f9eeb6618a70a7fc029f
tgambet <devnull@localhost>
2012-02-16 17:17:05 -0500

    + added a broadcast method that takes an Iterable or ObservationUpdates and broadcast them all

--- app/observer/Subscriber.scala
+++ app/observer/Subscriber.scala
@@ -14,6 +14,7 @@ trait Subscriber {
   def subscribe(): Unit
   def unsubscribe(): Unit
   def broadcast(msg: message.ObservationUpdate): Unit
+  def broadcast(msgs: Iterable[message.ObservationUpdate]): Unit
 }
 
 // TODO to be moved
@@ -26,5 +27,7 @@ class SubscriberImpl(observer: Observer) extends Subscriber {
   def unsubscribe(): Unit = observer.unsubscribe(this)
   
   def broadcast(msg: message.ObservationUpdate): Unit = enumerator.push(msg)
+  
+  def broadcast(msgs: Iterable[message.ObservationUpdate]): Unit = for(msg <- msgs) { enumerator.push(msg) }
   
 }
