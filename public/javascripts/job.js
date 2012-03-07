2c2f33715d667ba720e420f4fda7acb0adcc5c73
tgambet <devnull@localhost>
2012-03-07 10:02:40 -0500

    ~ some initial js objects that i'm forced to commit to be able to rebase

--- public/javascripts/job.js
+++ public/javascripts/job.js
@@ -1,19 +1,53 @@
 var Job = Backbone.Model.extend({
 	
 	defaults: {
+		"id": "",
 		"name": "",
-		"seeUri": "",
+		"seedUri": "",
+		"distance": "",
 		"status": "",
 		"lastRun": "",
 		"lastUpdated": "",
 		"errors": 0,
-		"warnings": 0
+		"warnings": 0,
+		"messages": new Messages()
 	},
 	
 	initialize: function() {
 		
 	}
 	
+});
+
+Functional.install();
+
+var Message = Backbone.Model.extend({
+	
+	url: "",
+	
+	defaults: {
+		"id": "",
+		"type": 0,
+		"timestamp": "",
+		"assertorId": "",
+		"line": 0,
+		"column": 0,
+		"context": "",
+		"message": ""
+	},
+	
+	initialize: function() {
+		
+	}
+
+});
+
+var Jobs = Backbone.Collection.extend({
+	model: Job
+});
+
+var Messages = Backbone.Collection.extend({
+	model: Message
 });
 
 var t = new Job();
