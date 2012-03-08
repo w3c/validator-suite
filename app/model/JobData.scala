58ab82a365d5badec218c79215613bcb85edff78
tgambet <devnull@localhost>
2012-03-07 21:28:18 -0500

    - import org.w3.vs.prod._

--- app/model/JobData.scala
+++ app/model/JobData.scala
@@ -2,7 +2,6 @@ package org.w3.vs.model
 
 import java.util.UUID
 import org.joda.time.DateTime
-import org.w3.vs.prod._
 import org.w3.vs.run._
 import akka.dispatch.Await
 import akka.pattern.AskTimeoutException
--- app/views/Helper.scala
+++ app/views/Helper.scala
@@ -1,7 +1,6 @@
 package views
 
 import org.w3.vs.model.Job
-import org.w3.vs.prod._
 import akka.dispatch.Await
 import akka.util.duration._
 import org.w3.vs.run.Stopped
@@ -9,18 +8,5 @@ import akka.pattern.AskTimeoutException
 import org.w3.vs.run.NotYetStarted
 
 object Helper {
-  
-  // moved to job
-//  def getJobStatus(id: Job#Id): org.w3.vs.run.RunStatus = {
-//    configuration.runCreator.byJobId(id).fold(
-//      failure => NotYetStarted,
-//      success => 
-//        try {
-//          Await.result(success.status, 100 milliseconds)
-//        } catch {
-//          case e: AskTimeoutException => {println(e); Stopped}
-//        }
-//    )
-//  }
   
 }
