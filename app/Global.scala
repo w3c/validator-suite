cf2f75465afc704bab2119aa97c77b3cd7f5df4e
tgambet <devnull@localhost>
2012-03-09 01:01:12 -0500

    + another default dev job

--- app/Global.scala
+++ app/Global.scala
@@ -29,7 +29,19 @@ object Global extends GlobalSettings {
         linkCheck=false,
         filter=Filter(include=Everything, exclude=Nothing)))
     
+    val ibm = Job(
+      name = "IBM",
+      creator = tgambet.id,
+      organization = w3c.id,
+      strategy = new EntryPointStrategy(
+        name="irrelevantForV1",
+        entrypoint=URL("http://www.ibm.com/"),
+        distance=1,
+        linkCheck=false,
+        filter=Filter(include=Everything, exclude=Nothing)))
+    
     store.putJob(job)
+    store.putJob(ibm)
     store.saveUser(User(email = "tgambet@w3.org", name = "Thomas Gambet", password = "secret", organization = w3c.id))
     store.saveUser(User(email = "bertails@w3.org", name = "Alexandre Bertails", password = "secret", organization = w3c.id))
   }
