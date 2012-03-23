290859d16c371478ae9f8cef721d09a752e9b455
tgambet <devnull@localhost>
2012-03-23 13:36:41 -0400

    ~ pass the user as an Option to the templates explicitly

--- app/controllers/Dashboard.scala
+++ app/controllers/Dashboard.scala
@@ -44,8 +44,7 @@ object Dashboard extends Controller {
           futureIterable.lift
         }.failMap(t => failWithGrace(Some(user))(StoreException(t)))
       } yield {
-        implicit val userSuccess = Success(user)
-        Ok(views.html.dashboard(viewInputs)())
+        Ok(views.html.dashboard(viewInputs, Some(user)))
       }
       futureResult.expiresWith(InternalServerError, 1, SECONDS)
     }
@@ -77,8 +76,7 @@ object Dashboard extends Controller {
           data <- job.jobData.lift failMap {t => failWithGrace(Some(user))(StoreException(t))}
           ars <- Job.getAssertorResults(job.id) failMap failWithGrace(Some(user))
         } yield {
-          implicit val userSuccess = Success(user)
-          Ok(views.html.job(job.configuration, data, ars))
+          Ok(views.html.job(job.configuration, data, ars, Some(user)))
         }
       futureResult.toPromise()
     }
@@ -95,13 +93,11 @@ object Dashboard extends Controller {
           user <- getAuthenticatedUser failMap failWithGrace(None)
           id <- {
             idOpt.toSuccess {
-              implicit val unkownUser = Failure(UnknownUser)
               Ok(views.html.jobForm(jobForm))
             }.toImmediateValidation
           }
           jobC <- getJobConfIfAllowed(user, id) failMap failWithGrace(Some(user))
         } yield {
-          implicit val userSuccess = Success(user)
           Ok(views.html.jobForm(jobForm.fill(jobC)))
         }
       futureResult.toPromise
@@ -130,8 +126,7 @@ object Dashboard extends Controller {
         for {
           user <- getAuthenticatedUser failMap failWithGrace(None)
           jobF <- isValidForm(jobForm).toImmediateValidation failMap { formWithErrors =>
-            implicit val userSuccess = Success(user)
-            BadRequest(views.html.jobForm(formWithErrors))
+            BadRequest(views.html.jobForm(formWithErrors, Some(user)))
           }
           id <- idOpt match {
             case None => {
@@ -212,8 +207,6 @@ object Dashboard extends Controller {
   }
 
   private def failWithGrace(authenticatedUserOpt: Option[User])(e: SuiteException)(implicit req: Request[_]): Result = {
-    implicit val userV: Validation[SuiteException, User] =
-      authenticatedUserOpt toSuccess UnknownUser
     e match {
       case UnknownJob => {
         if (isAjax) NotFound
@@ -225,15 +218,15 @@ object Dashboard extends Controller {
       }
       case UnauthorizedJob => {
         if (isAjax) Forbidden
-        else Forbidden(views.html.error(List(("error" -> "Forbidden"))))
+        else Forbidden(views.html.error(List(("error" -> "Forbidden")), authenticatedUserOpt))
       }
       case StoreException(t) => {
         if (isAjax) InternalServerError
-        else InternalServerError(views.html.error(List(("error" -> "Store Exception"))))
+        else InternalServerError(views.html.error(List(("error" -> "Store Exception")), authenticatedUserOpt))
       }
       case Timeout(t) => {
         if (isAjax) InternalServerError
-        else InternalServerError(views.html.error(List(("error" -> "Timeout Exception"))))
+        else InternalServerError(views.html.error(List(("error" -> "Timeout Exception")), authenticatedUserOpt))
       }
     }
   }
