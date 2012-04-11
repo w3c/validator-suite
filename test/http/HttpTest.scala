47083d38ba3f71148a7ea9cc8165f91be89fa220
Alexandre Bertails <bertails@w3.org>
2012-04-11 09:04:18 -0400

    ~ makes sure the test for foo.localhost gets executed only on Linux

--- test/http/HttpTest.scala
+++ test/http/HttpTest.scala
@@ -75,22 +75,25 @@ class HttpTest() extends RunTestHelper(new DefaultProdConfiguration { }) with In
 
   }
 
+  
+  if (System.getProperty("os.name") startsWith "Linux") {
 
-  "testing HEAD on non-existing domain (foo.localhost)" in {
+    "testing HEAD on non-existing domain (foo.localhost)" in {
+      
+      val newRunId = RunId.newId()
+      
+      http ! Fetch(URL("http://foo.localhost/bar"), HEAD, newRunId)
+      
+      val fetchResponse = expectMsgType[FetchResponse](1.second)
+      
+      inside (fetchResponse) { case KoResponse(url, action, why, runId) =>
+        url must be === (URL("http://foo.localhost/bar"))
+        action must be === (HEAD)
+        why.getClass.getName must be === ("java.net.ConnectException")
+        runId must be === newRunId
+      }
 
-    val newRunId = RunId.newId()
-
-    http ! Fetch(URL("http://foo.localhost/bar"), HEAD, newRunId)
-
-    val fetchResponse = expectMsgType[FetchResponse](1.second)
-
-    inside (fetchResponse) { case KoResponse(url, action, why, runId) =>
-      url must be === (URL("http://foo.localhost/bar"))
-      action must be === (HEAD)
-      why.getClass.getName must be === ("java.net.ConnectException")
-      runId must be === newRunId
     }
-
   }
 
   "testing delays" in {
