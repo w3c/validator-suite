9eb90d219fc705907c5a993e54b4d0c80584b795
tgambet <devnull@localhost>
2012-05-22 11:55:32 +0200

    ~ refactored FetchResponse, ResourceInfo and Response, as they were all the same thing. Kept the naming: Response / HttpResponse / ErrorResponse + created value objects and wrappers ~ refactored HttpAction and HttpVerb

--- app/model/ResourceInfo.scala
+++ app/model/ResourceInfo.scala
@@ -1,65 +1,107 @@
 package org.w3.vs.model
 
 import org.w3.util._
-import org.joda.time._
-import java.util.UUID
-import org.w3.vs.http._
 import org.w3.util.Headers.wrapHeaders
+import org.w3.vs.http._
+import org.joda.time._
+import scalaz.Equal
 
-object ResourceInfo {
+object Response {
 
   // TODO: make jobId part of fetchResponse
-  def fromFetchResponse(fetchResponse: FetchResponse, jobId: JobId): ResourceInfo = {
-    fetchResponse match {
-      case OkResponse(url, action, status, headers, body, runId) => {
-        val extractedURLs = headers.mimetype collect {
-          case "text/html" | "application/xhtml+xml" => URLExtractor.fromHtml(url, body).distinct
-          case "text/css" => URLExtractor.fromCSS(url, body).distinct
-        } getOrElse List.empty
-        val ri = ResourceInfo(
-          url = url,
-          jobId = jobId,
-          runId = runId,
-          action = action,
-          result = FetchResult(status, headers, extractedURLs))
-        ri
-      }
-      case KoResponse(url, action, why, runId) => {
-        val ri = ResourceInfo(
-          url = url,
-          jobId = jobId,
-          runId = runId,
-          action = action,
-          result = ResourceInfoError(why.getMessage))
-        ri
-      }
-    }
-  }
+//  def fromResponse(fetchResponse: Response, jobId: JobId): ResourceInfo = {
+//    fetchResponse match {
+//      case HttpResponse(url, action, status, headers, body, runId) => {
+//        val extractedURLs = headers.mimetype collect {
+//          case "text/html" | "application/xhtml+xml" => URLExtractor.fromHtml(url, body).distinct
+//          case "text/css" => URLExtractor.fromCSS(url, body).distinct
+//        } getOrElse List.empty
+//        val ri = FetchResult(FetchResultVO(
+//          jobId = jobId,
+//          runId = runId,
+//          url = url,
+//          action = action,
+//          status = status,
+//          headers = headers,
+//          extractedURLs = extractedURLs))
+//        ri
+//      }
+//      case ErrorResponse(url, action, why, runId) => {
+//        val ri = ResourceError(ResourceErrorVO(
+//          jobId = jobId,
+//          runId = runId,
+//          url = url,
+//          action = action,
+//          why = why.getMessage))
+//        ri
+//      }
+//    }
+//  }
 
 }
 
-case class ResourceInfo(
-    id: ResourceInfo#Id = UUID.randomUUID(),
-    url: URL,
+sealed trait ResponseVO {
+  val id: ResponseId = ResponseId()
+  val jobId: JobId
+  val runId: RunId
+  val url: URL
+  val action: HttpAction
+  val timestamp: DateTime = DateTime.now
+}
+
+case class ErrorResponseVO(
+    id: ResponseId = ResponseId(),
     jobId: JobId,
     runId: RunId,
-    action: HttpVerb,
-    timestamp: DateTime = new DateTime,
-    result: ResourceInfoResult) {
-  type Id = UUID
-  
-  def toTinyString: String = "[%s/%s\t%s\t%s\t%s" format (jobId.shortId, runId.shortId, action.toString, url.toString, timestamp.toString())
-  
-}
-
-
-
-sealed trait ResourceInfoResult
-
-case class ResourceInfoError(why: String) extends ResourceInfoResult
-
-case class FetchResult(
+    url: URL,
+    action: HttpAction,
+    timestamp: DateTime = DateTime.now,
+    why: String) extends ResponseVO
+    
+case class HttpResponseVO(
+    id: ResponseId = ResponseId(),
+    jobId: JobId,
+    runId: RunId,
+    url: URL,
+    action: HttpAction,
+    timestamp: DateTime = DateTime.now,
     status: Int,
     headers: Headers,
-    extractedLinks: List[URL]) extends ResourceInfoResult
+    extractedLinks: List[URL]) extends ResponseVO
 
+
+sealed trait Response {
+  val valueObject: ResponseVO
+  
+  def id: ResponseId = valueObject.id
+  def url: URL = valueObject.url
+  def action: HttpAction = valueObject.action
+  def timestamp: DateTime = valueObject.timestamp
+  
+  def getJob: FutureVal[Exception, Job] = Job.get(valueObject.jobId)
+  def getRun: FutureVal[Exception, Run] = Run.get(valueObject.runId)
+  
+  def toTinyString: String = "[%s/%s\t%s\t%s\t%s" format (valueObject.jobId.shortId, valueObject.runId.shortId, action.toString, url.toString, timestamp.toString())
+}
+
+case class ErrorResponse(valueObject: ErrorResponseVO) extends Response {
+  def why: String = valueObject.why
+}
+
+case class HttpResponse(valueObject: HttpResponseVO) extends Response {
+  def status: Int = valueObject.status
+  def headers: Headers = valueObject.headers 
+  def extractedLinks: List[URL] = valueObject.extractedLinks 
+} 
+
+
+sealed trait HttpAction
+case object IGNORE extends HttpAction
+case object GET extends HttpAction
+case object HEAD extends HttpAction
+
+object HttpAction {
+  implicit val equalHttpAction: Equal[HttpAction] = new Equal[HttpAction] {
+    def equal(left: HttpAction, right: HttpAction): Boolean = left == right
+  }
+}
