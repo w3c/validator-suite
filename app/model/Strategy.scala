5f356450892b81e98ea5486a2d30c93c1d1b3af9
tgambet <devnull@localhost>
2012-05-23 18:16:52 +0200

    + User and Strategy value object and wrappers

--- app/model/Strategy.scala
+++ app/model/Strategy.scala
@@ -1,11 +1,28 @@
 package org.w3.vs.model
 
 import org.w3.util._
-import java.util.UUID
 import org.w3.vs.assertor._
 import org.w3.util._
 import org.w3.util.Headers._
 import scalaz.Scalaz._
+import akka.dispatch._
+
+object Strategy {
+  
+  def apply(
+      id: StrategyId = StrategyId(),
+      entrypoint: URL,
+      distance: Int,
+      linkCheck: Boolean,
+      maxNumberOfResources: Int,
+      filter: Filter,
+      assertorsFor: AssertorSelector = AssertorSelector.simple): Strategy = 
+    Strategy(StrategyVO(id, entrypoint, distance, linkCheck, maxNumberOfResources, filter), assertorsFor)
+  
+  def get(id: StrategyId): FutureVal[Exception, Strategy] = sys.error("")
+  def save(strategy: Strategy)(implicit context: ExecutionContext): FutureVal[Exception, Strategy] = sys.error("")
+  
+}
 
 /** A [[org.w3.vs.model.ActionStrategy]] made of an entry point URL and a distance from it.
   * 
@@ -16,15 +33,24 @@ import scalaz.Scalaz._
   * @param distance the maximum distance the crawler should move away from `entrypoint`
   * @param filter a filter to be applied to the URLs being discovered
   */
-case class Strategy(
+case class StrategyVO(
     id: StrategyId = StrategyId(),
-    name: String,
     entrypoint: URL,
     distance: Int,
     linkCheck: Boolean,
     maxNumberOfResources: Int,
-    filter: Filter,
+    filter: Filter)
+
+case class Strategy(
+    valueObject: StrategyVO,
     assertorsFor: AssertorSelector = AssertorSelector.simple) {
+  
+  def id: StrategyId = valueObject.id
+  def entrypoint: URL = valueObject.entrypoint
+  def distance: Int = valueObject.distance
+  def linkCheck: Boolean = valueObject.linkCheck
+  def maxNumberOfResources: Int = valueObject.maxNumberOfResources
+  def filter: Filter = valueObject.filter
   
   val mainAuthority: Authority = entrypoint.authority
   
@@ -38,9 +64,9 @@ case class Strategy(
       else if (linkCheck)
         HEAD
       else
-        FetchNothing
+        IGNORE
     } else {
-      FetchNothing
+      IGNORE
     }
 
   def noAssertor(): Strategy = this.copy(assertorsFor = AssertorSelector.noAssertor)
--- app/model/User.scala
+++ app/model/User.scala
@@ -9,26 +9,57 @@ import akka.dispatch._
 import org.w3.vs.exception._
 
 object User {
-
-  def fake: User =
-    User(organization = OrganizationData.fake.id, email = "foo@bar.com", name = "foo", password = "bar")
-
+  
+  def apply(
+      id: UserId = UserId(),
+      name: String,
+      email: String,
+      password: String,
+      organizationId: OrganizationId): User =
+    User(UserVO(id, name, email, password, organizationId))
+  
+  def get(id: UserId): FutureVal[Exception, User] = sys.error("")
+  def getForOrganization(id: OrganizationId): FutureVal[Exception, Iterable[User]] = sys.error("") 
+  
   // TODO: For now these only fail with StoreExceptions but should also fail with a Unauthorized exception 
-  def authenticate(email: String, password: String)(implicit configuration: VSConfiguration, context: ExecutionContext): FutureValidation[SuiteException, User, Nothing, NOTSET] = {
-    import configuration.store
-    store.authenticate(email, password)
+  def authenticate(email: String, password: String)(implicit configuration: VSConfiguration): FutureVal[Exception, User] = {
+    /*import configuration.store
+    store.getUsers(email = Some(email), password = Some(password)).map(jobs => jobs.headOption).pureFold(
+      f => Failure(f),
+      {
+        case Some(s) => Success(s)
+        case _ => Failure(UnknownUser)
+      }
+    )*/
+    sys.error("")
   }
   
-  def getByEmail(email: String)(implicit configuration: VSConfiguration, context: ExecutionContext): FutureValidation[SuiteException, User, Nothing, NOTSET] = {
-    import configuration.store
-    store.getUserByEmail(email)
+  def getByEmail(email: String)(implicit configuration: VSConfiguration): FutureVal[Exception, User] = {
+    /*import configuration.store
+    store.getUsers(email = Some(email)).map(jobs => jobs.headOption).pureFold(
+      f => Failure(f),
+      {
+        case Some(s) => Success(s)
+        case _ => Failure(UnknownUser)
+      }
+    )*/
+    sys.error("")
   }
 
 }
 
-case class User(
+case class UserVO(
     id: UserId = UserId(),
-    organization: OrganizationId,
-    email: String,
     name: String,
-    password: String)
+    email: String,
+    password: String,
+    organizationId: OrganizationId)
+
+case class User(valueObject: UserVO) {
+  def id: UserId = valueObject.id
+  def name: String = valueObject.name
+  def email: String = valueObject.email
+  def password: String = valueObject.password
+  
+  def getOrganization: FutureVal[Exception, Organization] = sys.error("") 
+}
