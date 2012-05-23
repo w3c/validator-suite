49f2cc834903e435770bc3ec17efbda713bb78cc
tgambet <devnull@localhost>
2012-05-23 17:56:25 +0200

    ~ moved AssertorResponse to own file + closed wrappers of Assertion(+contexts) and AssertorResult(+assertions)

--- app/model/Assertion.scala
+++ app/model/Assertion.scala
@@ -5,77 +5,6 @@ import org.joda.time._
 import org.w3.vs.assertor._
 import scalaz.Validation
 
-sealed trait AssertorResponse {
-  val id: AssertorResponseId
-  val jobId: JobId
-  val runId: RunId
-  val assertorId: AssertorId
-  val sourceUrl: URL
-  val timestamp: DateTime
-}
-
-// not a persisted object for now
-case class AssertorFailure(
-    id: AssertorResponseId = AssertorResponseId(),
-    jobId: JobId,
-    runId: RunId,
-    assertorId: AssertorId,
-    sourceUrl: URL,
-    timestamp: DateTime = new DateTime,
-    why: String) extends AssertorResponse
-
-/** 
- *  AssertorResult 
- */
-case class AssertorResultVO(
-    id: AssertorResponseId = AssertorResponseId(),
-    jobId: JobId,
-    runId: RunId,
-    assertorId: AssertorId,
-    sourceUrl: URL,
-    timestamp: DateTime = DateTime.now)
-
-// closed with number of errors and warnings
-case class AssertorResult(
-    valueObject: AssertorResultVO,
-    errors: Int,
-    warnings: Int) extends AssertorResponse {
-  
-  def id = valueObject.id
-  def sourceUrl = valueObject.sourceUrl
-  def timestamp = valueObject.timestamp
-  
-  //def isValid = ! hasError
-  //def hasError: Boolean = assertions exists {_.severity == Error}
-  //def hasWarnings: Boolean = assertions exists {_.severity == Warning}
-  //def numberOfErrors = assertions.filter(_.severity == Error).size
-  //def numberOfWarnings = assertions.filter(_.severity == Warning).size
-  
-  def getJob: FutureVal[Exception, Job] = sys.error("ni")
-  def getRun: FutureVal[Exception, Run] = sys.error("ni")
-  def getAssertor: FutureVal[Exception, Assertor] = sys.error("ni")
-  def getAssertions: FutureVal[Exception, Iterable[Assertion]] = Assertion.getForResponse(id)
-  def save(): FutureVal[Exception, AssertorResult] = AssertorResult.save(this)
-}
-
-object AssertorResult {
-  
-  def apply(
-    id: AssertorResponseId = AssertorResponseId(),
-    jobId: JobId,
-    runId: RunId,
-    assertorId: AssertorId,
-    sourceUrl: URL,
-    timestamp: DateTime = DateTime.now,
-    errors: Int,
-    warnings: Int) = new AssertorResult(AssertorResultVO(id, jobId, runId, assertorId, sourceUrl, timestamp), errors, warnings)
-  
-  def get(id: AssertorResponseId): FutureVal[Exception, AssertorResult] = sys.error("ni")
-  def getForJob(id: JobId, after: Option[DateTime] = None): FutureVal[Exception, Iterable[AssertorResult]] = sys.error("ni")
-  def getForURL(url: URL): FutureVal[Exception, Iterable[AssertorResult]] = sys.error("ni")
-  def save(result: AssertorResult): FutureVal[Exception, AssertorResult] = sys.error("ni")
-}
-
 /** 
  *  Assertion
  *  
@@ -106,6 +35,8 @@ case class Assertion(valueObject: AssertionVO) {
   def getContexts: FutureVal[Exception, Seq[Context]] 
   
 }
+
+case class AssertionClosed(assertion: Assertion, contexts: Iterable[Context])
 
 object Assertion {
   
---
+++ app/model/AssertorResponse.scala
@@ -0,0 +1,79 @@
+package org.w3.vs.model
+
+import org.w3.util._
+import org.joda.time._
+import org.w3.vs.assertor._
+import scalaz.Validation
+
+sealed trait AssertorResponse {
+  val id: AssertorResponseId
+  val jobId: JobId
+  val runId: RunId
+  val assertorId: AssertorId
+  val sourceUrl: URL
+  val timestamp: DateTime
+}
+
+// not a persisted object for now
+case class AssertorFailure(
+    id: AssertorResponseId = AssertorResponseId(),
+    jobId: JobId,
+    runId: RunId,
+    assertorId: AssertorId,
+    sourceUrl: URL,
+    timestamp: DateTime = new DateTime,
+    why: String) extends AssertorResponse
+
+/** 
+ *  AssertorResult 
+ */
+case class AssertorResultVO(
+    id: AssertorResponseId = AssertorResponseId(),
+    jobId: JobId,
+    runId: RunId,
+    assertorId: AssertorId,
+    sourceUrl: URL,
+    timestamp: DateTime = DateTime.now)
+
+// closed with number of errors and warnings
+case class AssertorResult(
+    valueObject: AssertorResultVO,
+    errors: Int,
+    warnings: Int) extends AssertorResponse {
+  
+  def id = valueObject.id
+  def sourceUrl = valueObject.sourceUrl
+  def timestamp = valueObject.timestamp
+  
+  //def isValid = ! hasError
+  //def hasError: Boolean = assertions exists {_.severity == Error}
+  //def hasWarnings: Boolean = assertions exists {_.severity == Warning}
+  //def numberOfErrors = assertions.filter(_.severity == Error).size
+  //def numberOfWarnings = assertions.filter(_.severity == Warning).size
+  
+  def getJob: FutureVal[Exception, Job] = sys.error("ni")
+  def getRun: FutureVal[Exception, Run] = sys.error("ni")
+  def getAssertor: FutureVal[Exception, Assertor] = sys.error("ni")
+  def getAssertions: FutureVal[Exception, Iterable[Assertion]] = Assertion.getForResponse(id)
+  def save(): FutureVal[Exception, AssertorResult] = AssertorResult.save(this)
+}
+
+case class AssertorResultClosed(assertorResult: AssertorResult, assertionsClosed: Iterable[AssertionClosed])
+
+object AssertorResult {
+  
+  def apply(
+    id: AssertorResponseId = AssertorResponseId(),
+    jobId: JobId,
+    runId: RunId,
+    assertorId: AssertorId,
+    sourceUrl: URL,
+    timestamp: DateTime = DateTime.now,
+    errors: Int,
+    warnings: Int) = new AssertorResult(AssertorResultVO(id, jobId, runId, assertorId, sourceUrl, timestamp), errors, warnings)
+  
+  def get(id: AssertorResponseId): FutureVal[Exception, AssertorResult] = sys.error("ni")
+  def getForJob(id: JobId, after: Option[DateTime] = None): FutureVal[Exception, Iterable[AssertorResult]] = sys.error("ni")
+  def getForURL(url: URL): FutureVal[Exception, Iterable[AssertorResult]] = sys.error("ni")
+  def save(result: AssertorResult): FutureVal[Exception, AssertorResult] = sys.error("ni")
+}
