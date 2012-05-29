c8f15f2d386f85671b783f23baa06396e384cae6
Alexandre Bertails <bertails@w3.org>
2012-05-29 11:35:49 -0400

    ~ moved uri builders in their own trait (the only purpose is to get mixed in the Binders class)

--- app/model/Binders.scala
+++ app/model/Binders.scala
@@ -1,6 +1,5 @@
 package org.w3.vs.model
 
-import org.w3.vs.model._
 import org.w3.banana._
 import org.w3.banana.diesel._
 import scalaz._
@@ -14,7 +13,8 @@ import org.joda.time.DateTime
 case class Binders[Rdf <: RDF](
   ops: RDFOperations[Rdf],
   union: GraphUnion[Rdf],
-  graphTraversal: RDFGraphTraversal[Rdf]) {
+  graphTraversal: RDFGraphTraversal[Rdf])
+extends UriBuilders[Rdf] {
 
   val diesel: Diesel[Rdf] = Diesel(ops, union, graphTraversal)
   
@@ -32,57 +32,6 @@ case class Binders[Rdf <: RDF](
 
   implicit def addIfDefinedMethod[S](s: S): IfDefined[S] = new IfDefined[S](s)
 
-  /* uri builders for entities */
-
-  object AssertionUri extends PrefixBuilder("", "https://validator.w3.org/suite/assertion/", ops) {
-    def apply(id: AssertionId): Rdf#URI = apply(id.toString)
-    def getId(uri: Rdf#URI): Validation[BananaException, AssertionId] = getLocalName(uri) map AssertionId.apply
-  }
-
-  object ContextUri extends PrefixBuilder("", "https://validator.w3.org/suite/context/", ops) {
-    def apply(id: ContextId): Rdf#URI = apply(id.toString)
-    def getId(uri: Rdf#URI): Validation[BananaException, ContextId] = getLocalName(uri) map ContextId.apply
-  }
-
-  object AssertorResultUri extends PrefixBuilder("", "https://validator.w3.org/suite/assertor-result/", ops) {
-    def apply(id: AssertorResponseId): Rdf#URI = apply(id.toString)
-    def getId(uri: Rdf#URI): Validation[BananaException, AssertorResponseId] = getLocalName(uri) map AssertorResponseId.apply
-  }
-
-  object JobUri extends PrefixBuilder("", "https://validator.w3.org/suite/job/", ops) {
-    def apply(id: JobId): Rdf#URI = apply(id.toString)
-    def getId(uri: Rdf#URI): Validation[BananaException, JobId] = getLocalName(uri) map JobId.apply
-  }
-
-  object JobDataUri extends PrefixBuilder("", "https://validator.w3.org/suite/job-data/", ops) {
-    def apply(id: JobDataId): Rdf#URI = apply(id.toString)
-    def getId(uri: Rdf#URI): Validation[BananaException, JobDataId] = getLocalName(uri) map JobDataId.apply
-  }
-
-  object OrganizationUri extends PrefixBuilder("", "https://validator.w3.org/suite/organization/", ops) {
-    def apply(id: OrganizationId): Rdf#URI = apply(id.toString)
-    def getId(uri: Rdf#URI): Validation[BananaException, OrganizationId] = getLocalName(uri) map OrganizationId.apply
-  }
-
-  object ResourceResponseUri extends PrefixBuilder("", "https://validator.w3.org/suite/resource-response/", ops) {
-    def apply(id: ResourceResponseId): Rdf#URI = apply(id.toString)
-    def getId(uri: Rdf#URI): Validation[BananaException, ResourceResponseId] = getLocalName(uri) map ResourceResponseId.apply
-  }
-
-  object RunUri extends PrefixBuilder("", "https://validator.w3.org/suite/run/", ops) {
-    def apply(id: RunId): Rdf#URI = apply(id.toString)
-    def getId(uri: Rdf#URI): Validation[BananaException, RunId] = getLocalName(uri) map RunId.apply
-  }
-
-  object StrategyUri extends PrefixBuilder("", "https://validator.w3.org/suite/strategy/", ops) {
-    def apply(id: StrategyId): Rdf#URI = apply(id.toString)
-    def getId(uri: Rdf#URI): Validation[BananaException, StrategyId] = getLocalName(uri) map StrategyId.apply
-  }
-
-  object UserUri extends PrefixBuilder("", "https://validator.w3.org/suite/user/", ops) {
-    def apply(id: UserId): Rdf#URI = apply(id.toString)
-    def getId(uri: Rdf#URI): Validation[BananaException, UserId] = getLocalName(uri) map UserId.apply
-  }
 
   /* ontologies for entities */
 
