69635dbf005ea6424cd6c4d0985ea648e7cfdf19
Alexandre Bertails <bertails@w3.org>
2012-06-04 12:17:17 -0400

    ~ fixed conflicts between jetty jars, would be better to fix in css-validator-standalone

--- project/Build.scala
+++ project/Build.scala
@@ -17,11 +17,15 @@ object ApplicationBuild extends Build {
     "com.novus" %% "salat-core" % "0.0.8-SNAPSHOT",
     "org.scalaz" %% "scalaz-core" % "7.0-SNAPSHOT",
     "org.w3" %% "banana-jena" % "x01-SNAPSHOT" changing(),
-    "org.w3" %% "css-validator-standalone" % "1.0-SNAPSHOT",
+    "org.w3" %% "css-validator-standalone" % "1.0-SNAPSHOT" intransitive(),
     // test dependencies
     "com.typesafe.akka" % "akka-testkit" % "2.0.1" % "test",
-    "net.databinder" %% "unfiltered-filter" % "0.5.3" % "test",
-    "net.databinder" %% "unfiltered-jetty" % "0.5.3" % "test",
+    "net.databinder" %% "unfiltered-filter" % "0.6.2" % "test",
+    "net.databinder" %% "unfiltered-jetty" % "0.6.2" % "test",
+    // "net.databinder" %% "unfiltered-filter" % "0.6.2" % "test" intransitive(),
+    // "net.databinder" %% "unfiltered-jetty" % "0.6.2" % "test" intransitive(),
+    // "net.databinder" %% "unfiltered-util" % "0.6.2" % "test" intransitive(),
+    // "net.databinder" %% "unfiltered" % "0.6.2" % "test" intransitive(),
     "org.scalatest" %% "scalatest" % "1.7.1" % "test"
   )
 
