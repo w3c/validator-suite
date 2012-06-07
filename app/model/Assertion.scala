92b46bf67c73ac8a432920eab0a0205a9c245474
tgambet <devnull@localhost>
2012-06-07 15:46:38 +0200

    + Assertion.save & Context.save

--- app/model/Assertion.scala
+++ app/model/Assertion.scala
@@ -21,24 +21,20 @@ case class Assertion(
   //def getAssertorResult: FutureVal[Exception, AssertorResult] = AssertorResult.get(assertorResponseId)
   def getContexts: FutureVal[Exception, Iterable[Context]] = Context.getForAssertion(id) 
   def toValueObject: AssertionVO = AssertionVO(id, jobId, runId, assertorId, url, lang, title, severity, description, timestamp)
+  def save(): FutureVal[Exception, Unit] = Assertion.save(this)
 }
 
 object Assertion {
   def get(id: AssertionId)(implicit conf: VSConfiguration): FutureVal[Exception, Assertion] = sys.error("ni")
   def getForJob(id: JobId)(implicit conf: VSConfiguration): FutureVal[Exception, Iterable[Assertion]] = {
-    
     implicit def ec = conf.webExecutionContext
-    FutureVal.successful(Iterable(
-      Assertion.apply(AssertionId(), JobId(), RunId(), AssertorId(), 
-          URL("http://www.w3.org"), "en", "T", Error, None),
-      Assertion.apply(AssertionId(), JobId(), RunId(), AssertorId(), 
-          URL("http://www.w3.org"), "en", "T", Warning, None),
-      Assertion.apply(AssertionId(), JobId(), RunId(), AssertorId(), 
-          URL("http://www.w3.org/TR"), "en", "T", Warning, None)
-    ))
-    
+    FutureVal.successful(Iterable())
   }
   def getForRun(id: RunId)(implicit conf: VSConfiguration): FutureVal[Exception, Iterable[Assertion]] = sys.error("ni")
+  def save(assertion: Assertion)(implicit conf: VSConfiguration): FutureVal[Exception, Unit] = {
+    implicit def ec = conf.webExecutionContext
+    FutureVal.successful(())
+  }
 }
 
 case class AssertionClosed(assertion: Assertion, contexts: Iterable[Context])
@@ -70,6 +66,7 @@ case class Context(
   
   def getAssertion: FutureVal[Exception, Assertion] = Assertion.get(assertionId)
   def toValueObject: ContextVO = ContextVO(id, content, line, column, assertionId)
+  def save(): FutureVal[Exception, Unit] = Context.save(this)
 } 
 
 object Context {
@@ -83,4 +80,8 @@ object Context {
   
   def get(id: ContextId)(implicit conf: VSConfiguration): FutureVal[Exception, Context] = sys.error("ni")
   def getForAssertion(id: AssertionId)(implicit conf: VSConfiguration): FutureVal[Exception, Iterable[Context]] = sys.error("ni")
+  def save(context: Context)(implicit conf: VSConfiguration): FutureVal[Exception, Unit] = {
+    implicit def ec = conf.webExecutionContext
+    FutureVal.successful(())
+  }
 }
