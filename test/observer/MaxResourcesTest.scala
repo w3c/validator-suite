763d5b169811cdceb99206b7e5cc01a0861d1ae1
Alexandre Bertails <bertails@w3.org>
2012-06-13 11:50:22 -0400

    ~ uses fishForMessage in MaxResourcesTest and rehabilited full test

--- test/observer/MaxResourcesTest.scala
+++ test/observer/MaxResourcesTest.scala
@@ -16,11 +16,13 @@ import org.w3.vs.http._
 
 class MaxResourcesTest extends RunTestHelper(new DefaultProdConfiguration { }) with TestKitHelper {
 
+  val maxResources = 100
+
   val strategy =
     Strategy(
       entrypoint=URL("http://localhost:9001/"),
       linkCheck=true,
-      maxResources = 100,
+      maxResources = maxResources,
       filter=Filter(include=Everything, exclude=Nothing)).noAssertor()
   
   val job = Job(strategy = strategy, creatorId = userTest.id, organizationId = organizationTest.id, name = "@@")
@@ -28,21 +30,26 @@ class MaxResourcesTest extends RunTestHelper(new DefaultProdConfiguration { }) w
   val servers = Seq(Webserver(9001, Website.tree(4).toServlet))
 
   "shoudldn't access more that 100 resources" in {
-    //stores.OrganizationStore.put(organizationTest)
-    //store.putJob(job).waitResult()
+
     (for {
       a <- Organization.save(organizationTest)
       b <- Job.save(job)
-    } yield ()).await(5 seconds)
+    } yield ()).await(5.second)
+
     PathAware(http, http.path / "localhost_9001") ! SetSleepTime(0)
+
     job.run()
-    //job.listen(testActor)
-    /*fishForMessagePF(3.seconds) {
-      case UpdateData(jobData) if jobData.activity == Idle => {
-        val resources = store.listResourceInfos(job.id).waitResult()
-        resources must have size (100)
+
+    job.listen(testActor)
+
+    fishForMessagePF(3.seconds) {
+      case UpdateData(_, activity) if activity == Idle => {
+        val run = job.getRun().result(1.second) getOrElse sys.error("getRun")
+        val rrs = ResourceResponse.getForRun(run.id).result(1.second) getOrElse sys.error("getForRun")
+        rrs must have size (maxResources)
       }
-    }*/
+    }
+
   }
   
 }
