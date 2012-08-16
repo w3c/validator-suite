5d082cb7149d08a101e1cccfaaf2e35f005ba312
Thomas Gambet <thomas.gambet@gmail.com>
2012-08-17 00:37:10 +0200

    reimplemented grouping assertions based on the message title

--- app/assertor/FromHttpResponseAssertor.scala
+++ app/assertor/FromHttpResponseAssertor.scala
@@ -8,6 +8,12 @@ trait FromHttpResponseAssertor extends FromURLAssertor {
   def assert(context: (OrganizationId, JobId, RunId), response: HttpResponse): FutureVal[AssertorFailure, AssertorResult] = 
     FutureVal {
       val assertions = assert(response.url)
+          .groupBy{case a => a.url.toString + a.title}
+          .map(_._2)
+          .map {case assertions =>
+            val contexts = assertions.foldLeft(Iterable[Context]()){case (contexts, a) => contexts ++ a.contexts}
+            assertions.head.copy(contexts = contexts.toList)
+          }
       AssertorResult(context = context, assertorId = id, sourceUrl = response.url, assertions = assertions)
     } failMap { throwable =>
       AssertorFailure(context = context, assertorId = id, sourceUrl = response.url, why = throwable)
--- app/assertor/ValidatorNu.scala
+++ app/assertor/ValidatorNu.scala
@@ -29,24 +29,24 @@ object ValidatorNu extends FromHttpResponseAssertor {
   }
 
   /*def assert(url: URL)(implicit context: ExecutionContext): FutureVal[Throwable, Iterable[Assertion]] = FutureVal {
-	val urlCon = validatorURLForMachine(url).openConnection()
-	urlCon.setConnectTimeout(2000)
-	//urlCon.setReadTimeout(10000)
-	val content = Source.fromInputStream(urlCon.getInputStream).getLines.mkString("\n")*/
+    val urlCon = validatorURLForMachine(url).openConnection()
+    urlCon.setConnectTimeout(2000)
+    //urlCon.setReadTimeout(10000)
+    val content = Source.fromInputStream(urlCon.getInputStream).getLines.mkString("\n")*/
   def assert(source: Source): Iterable[Assertion] = {
     val json = Json.parse(source.getLines.mkString("\n"))
     val url = URL((json \ "url").asInstanceOf[JsString].value)
     val messages = json \ "messages"
     messages.asInstanceOf[JsArray].value.map { obj =>
-      val assertionId = AssertionId();
+      val assertionId = AssertionId()
       val severity = AssertionSeverity((obj \ "type") match {
-	case JsString("non-document-error") => throw new Exception("validator.nu failed");
-	case JsString("info") => (obj \ "subType") match {
-	  case JsString("warning") => "warning";
-	  case _ => "info"
-	}
-	case JsString(s) => s; 
-	case _ => throw new Exception("malformed json") // TODO
+        case JsString("non-document-error") => throw new Exception("validator.nu failed")
+        case JsString("info") => (obj \ "subType") match {
+          case JsString("warning") => "warning"
+          case _ => "info"
+        }
+        case JsString(s) => s
+        case _ => throw new Exception("malformed json") // TODO
       })
       val title = (obj \ "message") match {case JsString(s) => HtmlFormat.escape(s).text; case _ => throw new Exception("malformed json")}
       val lastLine = (obj \ "lastLine") match {case JsNumber(bigDec) => Some(bigDec.toInt); case _ => None}
@@ -54,7 +54,7 @@ object ValidatorNu extends FromHttpResponseAssertor {
       val extract = (obj \ "extract") match {case JsString(s) => Some(HtmlFormat.escape(s).text); case _ => None}
       val contexts = extract match {
         case Some(code) => List(Context(code.trim, lastLine, lastCol)) // The model needs to accept a range of lines/column
-	case _ => List()
+        case _ => List()
       }
       Assertion(url, id, contexts, "en", title, severity, None)
     }
