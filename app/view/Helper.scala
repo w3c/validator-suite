964eb16f5060b982da059188860779d23d1a810a
Thomas Gambet <thomas.gambet@gmail.com>
2012-08-18 22:56:22 +0200

    shorten long uris on the report

--- app/view/Helper.scala
+++ app/view/Helper.scala
@@ -8,5 +8,18 @@ object Helper {
   val TimeFormatter = org.joda.time.format.DateTimeFormat.forPattern("MM/dd/yy' at 'K:mma")
   def formatTime(time: DateTime): String = TimeFormatter.print(time).toLowerCase
   def encode(url: URL): String = java.net.URLEncoder.encode(url.toString, "utf-8")
+
+  def shorten(string: String, limit: Int): String = {
+    if (string.size > limit) {
+      val dif = string.size - limit
+      string.substring(0, (string.size - dif)/2) +
+        """…""" +
+        string.substring((string.size + dif)/2)
+    } else string
+  }
+
+  def shorten(url: URL, limit: Int): String = {
+    shorten(url.toString.replaceFirst("http://", ""), limit)
+  }
   
 }
