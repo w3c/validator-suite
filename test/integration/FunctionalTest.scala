bfa4fea50db100cb71e9b8c39abdf493c2a4ee2e
Thomas Gambet <thomas.gambet@gmail.com>
2012-09-12 13:24:11 +0200

    uncommented application.context (closes #64) and adapted FunctionalTest No other modifications are needed

--- test/integration/FunctionalTest.scala
+++ test/integration/FunctionalTest.scala
@@ -4,6 +4,8 @@ import play.api.test._
 import play.api.test.Helpers._
 import org.scalatest._
 import org.scalatest.matchers.MustMatchers
+import play.api.Play._
+import play.api.test.TestServer
 
 class FunctionalTest extends WordSpec with MustMatchers with BeforeAndAfterAll {
 
@@ -16,28 +18,32 @@ class FunctionalTest extends WordSpec with MustMatchers with BeforeAndAfterAll {
     running(TestServer(9001), HTMLUNIT) { browser =>
       Thread.sleep(2000)
       import browser._
-      
-      goTo("http://localhost:9001/dashboard")
-      url must be === ("http://localhost:9001/dashboard")
+
+      val baseUrl = "http://localhost:9001" + current.configuration.getString("application.context").get
+
+      println(baseUrl)
+
+      goTo(baseUrl + "/dashboard")
+      url must be === (baseUrl + "/dashboard")
       fill("#email").`with`("bertails@w3.org")
       fill("#password").`with`("secret")
       click("#submit-login")
-      url must be === ("http://localhost:9001/dashboard")
+      url must be === (baseUrl + "/dashboard")
 
       $("#admin .name").first().getText() must include ("Alexandre Bertails")       
       
       // and log out
       click("#admin form button")
       //goTo("http://localhost:9001/logout")
-      url must be === ("http://localhost:9001/login")
-      goTo("http://localhost:9001/dashboard")
-      $("form[action='/login']").isEmpty must be (false)
+      url must be === (baseUrl + "/login")
+      goTo(baseUrl + "/dashboard")
+      $("button#submit-login").isEmpty must be (false)
       
       // can't log in with wrong password
       fill("#email").`with`("bertails@w3.org")
       fill("#password").`with`("wrong")
       click("#submit-login")
-      url must be === ("http://localhost:9001/login")
+      url must be === (baseUrl + "/login")
 
     }
   }
