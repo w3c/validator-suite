5986d8f2602993663f2a76a909e578a96a96438b
Thomas Gambet <thomas.gambet@gmail.com>
2012-09-20 13:48:30 +0200

    fixed FunctionalTest to use the route file

--- test/integration/FunctionalTest.scala
+++ test/integration/FunctionalTest.scala
@@ -19,29 +19,29 @@ class FunctionalTest extends WordSpec with MustMatchers with BeforeAndAfterAll {
       Thread.sleep(2000)
       import browser._
 
-      val baseUrl = "http://localhost:9001" + current.configuration.getString("application.context").get
+      val baseUrl: String = "http://localhost:9001"
 
-      goTo(baseUrl + "/dashboard")
-      url must be === (baseUrl + "/dashboard")
+      goTo(baseUrl + controllers.routes.Jobs.index.toString)
+      url must be === (baseUrl + controllers.routes.Jobs.index.toString)
       fill("#email").`with`("bertails@w3.org")
       fill("#password").`with`("secret")
       click("#submit-login")
-      url must be === (baseUrl + "/dashboard")
+      url must be === (baseUrl + controllers.routes.Jobs.index.toString)
 
       $("#admin .name").first().getText() must include ("Alexandre Bertails")       
       
       // and log out
       click("#admin form button")
       //goTo("http://localhost:9001/logout")
-      url must be === (baseUrl + "/login")
-      goTo(baseUrl + "/dashboard")
+      url must be === (baseUrl + controllers.routes.Application.login.toString)
+      goTo(baseUrl + controllers.routes.Jobs.index.toString)
       $("button#submit-login").isEmpty must be (false)
       
       // can't log in with wrong password
       fill("#email").`with`("bertails@w3.org")
       fill("#password").`with`("wrong")
       click("#submit-login")
-      url must be === (baseUrl + "/login")
+      url must be === (baseUrl + controllers.routes.Application.login.toString)
 
     }
   }
