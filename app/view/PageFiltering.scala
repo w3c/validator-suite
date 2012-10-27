e64cc7dac36fe05a6224a482dc5c29f51f30177b
Thomas Gambet <thomas.gambet@gmail.com>
2012-10-27 19:34:28 +0200

    removes 15 files that have been refactored, removed redondant logger configurations

--- app/view/PageFiltering.scala
+++
@@ -1,8 +0,0 @@
-/*package org.w3.vs.view
-
-trait PageFiltering[A] {
-  def validate(filter: Option[String]): Option[String]
-  def filter(filter: Option[String]): (A) => Boolean
-  def search(search: Option[String]): (A) => Boolean
-}
-  */
