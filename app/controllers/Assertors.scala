afccb02d8248e640d4a7ea974ba14a2eaf1d3206
Alexandre Bertails <bertails@gmail.com>
2012-11-07 15:13:24 -0500

    added metrics for Assertors.*

--- app/controllers/Assertors.scala
+++ app/controllers/Assertors.scala
@@ -5,7 +5,11 @@ import org.w3.vs.model._
 import org.w3.vs.view.collection.AssertorsView
 import scala.concurrent.ExecutionContext.Implicits.global
 import org.w3.vs.controllers._
-import play.api.mvc.{Action, Handler}
+import play.api.mvc.{ Result, Action, Handler }
+import scala.concurrent.Future
+import org.w3.util.Util._
+import com.yammer.metrics.Metrics
+import java.util.concurrent.TimeUnit.{ MILLISECONDS, SECONDS }
 
 object Assertors extends VSController {
 
@@ -16,24 +20,32 @@ object Assertors extends VSController {
     case None => index(id)
   }
 
+  val indexName = (new controllers.javascript.ReverseAssertors).index.name
+  val indexTimer = Metrics.newTimer(Assertors.getClass, indexName, MILLISECONDS, SECONDS)
+
   def index(id: JobId) : ActionA = AuthAsyncAction { implicit req => user =>
-    for {
+    val f: Future[PartialFunction[Format, Result]] = for {
       job_ <- user.getJob(id)
       assertions_ <- job_.getAssertions()
       assertors = AssertorsView(assertions_)
     } yield {
       case Json => Ok(assertors.bindFromRequest.toJson)
     }
+    f.timer(indexName).timer(indexTimer)
   }
 
+  val indexUrlName = indexName + "+url"
+  val indexUrlTimer = Metrics.newTimer(Assertors.getClass, indexUrlName, MILLISECONDS, SECONDS)
+
   def index(id: JobId, url: URL) : ActionA = AuthAsyncAction { implicit req => user =>
-    for {
+    val f: Future[PartialFunction[Format, Result]] = for {
       job_ <- user.getJob(id)
       assertions_ <- job_.getAssertions().map(_.filter(_.url == url))
       assertors = AssertorsView(assertions_)
     } yield {
       case Json => Ok(assertors.bindFromRequest.toJson)
     }
+    f.timer(indexName).timer(indexTimer)
   }
 
   def socket(jobId: JobId, url: Option[URL], typ: SocketType): Handler = {
