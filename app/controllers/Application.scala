4d921ea7a6ae8977228b4870a23be63777ad5485
Alexandre Bertails <bertails@gmail.com>
2012-11-07 11:39:16 -0500

    added metrics for Application.*

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,9 +6,11 @@ import org.w3.vs.model._
 import org.w3.vs.view.form._
 import play.api.i18n._
 import play.api.mvc._
-import scala.Some
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import org.w3.util.Util._
+import com.yammer.metrics.Metrics
+import java.util.concurrent.TimeUnit.{ MILLISECONDS, SECONDS }
 
 object Application extends VSController {
   
@@ -16,23 +18,30 @@ object Application extends VSController {
 
   def index: ActionA = Action { Redirect(routes.Jobs.index.toString) }
 
+  val loginName = (new controllers.javascript.ReverseApplication).login.name
+  val loginTimer = Metrics.newTimer(Jobs.getClass, loginName, MILLISECONDS, SECONDS)
+
   def login: ActionA = Action { implicit req =>
     AsyncResult {
-      getUser map {
+      val f = getUser map {
         case _ => Redirect(routes.Jobs.index) // Already logged in -> redirect to index
       } recover {
         case  _: UnauthorizedException => Ok(views.html.login(LoginForm.blank)).withNewSession
       } recover toError
+      f.timer(loginName).timer(loginTimer)
     }
   }
   
   def logout: ActionA = Action {
     Redirect(routes.Application.login).withNewSession.flashing("success" -> Messages("application.loggedOut"))
   }
+
+  val authenticateName = (new controllers.javascript.ReverseApplication).authenticate.name
+  val authenticateTimer = Metrics.newTimer(Jobs.getClass, authenticateName, MILLISECONDS, SECONDS)
   
   def authenticate: ActionA = Action { implicit req =>
     AsyncResult {
-      (for {
+      val f = (for {
         form <- Future.successful(LoginForm.bind() match {
           case Left(form) => throw InvalidFormException(form)
           case Right(validForm) => validForm
@@ -51,6 +60,7 @@ object Application extends VSController {
         case  _: UnauthorizedException => Unauthorized(views.html.login(LoginForm.blank, List(("error", Messages("application.invalidCredentials"))))).withNewSession
         case InvalidFormException(form: LoginForm) => BadRequest(views.html.login(form))
       } recover toError
+      f.timer(authenticateName).timer(authenticateTimer)
     }
   }
   
