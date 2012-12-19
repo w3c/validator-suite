e984c08ff8128d3940a21d26d9765bb4e7449838
Thomas Gambet <thomas.gambet@gmail.com>
2012-12-19 15:38:07 +0100

    login form validation

--- app/view/form/LoginForm.scala
+++ app/view/form/LoginForm.scala
@@ -1,8 +1,8 @@
 package org.w3.vs.view.form
 
-import java.util.concurrent.TimeoutException
-import play.api.data.Forms._
 import play.api.data._
+import play.api.data.Forms._
+import play.api.data.validation.Constraints._
 import play.api.i18n.Messages
 import play.api.mvc._
 import scala.concurrent._
@@ -11,7 +11,6 @@ object LoginForm {
 
   def bind()(implicit req: Request[_], context: ExecutionContext): Either[LoginForm, ValidLoginForm] = {
     val form = playForm.bindFromRequest
-    implicit def onTo(to: TimeoutException): LoginForm = new LoginForm(form.withError("key", Messages("error.timeout")))
     form.fold(
       f => Left(new LoginForm(f)),
       s => Right(new ValidLoginForm(form, s))
@@ -22,8 +21,8 @@ object LoginForm {
 
   private def playForm: Form[(String, String)] = Form(
     tuple(
-      "email" -> email,
-      "password" -> text
+      "email" -> email.verifying(nonEmpty),
+      "password" -> nonEmptyText
     )
   )
 
@@ -33,7 +32,10 @@ class LoginForm private[view](form: Form[(String, String)]) extends VSForm {
 
   def apply(s: String) = form(s)
 
-  def errors: Seq[(String, String)] = form.errors.map{case error => ("error", /*error.key + */error.message)}
+  def errors: Seq[(String, String)] = form.errors.map{
+    case error => ("error", Messages("form." + error.key + "." + error.message))
+  }
+
 }
 
 class ValidLoginForm private[view](form: Form[(String, String)], bind: (String, String)) extends LoginForm(form) with VSForm {
