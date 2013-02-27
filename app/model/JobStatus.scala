42871115f234b03c12072e059149c30a930b1e1e
Alexandre Bertails <bertails@gmail.com>
2013-02-27 13:56:36 -0500

    removed some useless imports

--- app/model/JobStatus.scala
+++ app/model/JobStatus.scala
@@ -1,18 +1,8 @@
 package org.w3.vs.model
 
-import java.nio.channels.ClosedChannelException
 import org.joda.time.{ DateTime, DateTimeZone }
-import org.w3.util.akkaext._
-import akka.actor._
-import play.api.libs.iteratee._
-import play.Logger
-import org.w3.util._
-import scalaz.Equal
-import scalaz.Equal._
-import org.w3.vs._
-import org.w3.vs.actor.JobActor._
-import scala.concurrent.{ ops => _, _ }
-import scala.concurrent.ExecutionContext.Implicits.global
+import akka.actor.ActorPath
+import org.w3.util.URL
 
 sealed trait JobStatus
 
