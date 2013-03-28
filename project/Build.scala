fcd817b46df91142ca4742f1b22af51876af879f
Alexandre Bertails <bertails@gmail.com>
2013-03-28 17:16:25 -0400

    several fixes in validators.jar

--- project/Build.scala
+++ project/Build.scala
@@ -18,9 +18,8 @@ object ApplicationBuild extends Build {
     // runtime dependencies
     "org.scala-lang" % "scala-actors" % "2.10.1",
     "org.apache.commons" % "commons-lang3" % "3.1" intransitive(), // For StringUtils escaping functions
-    "nu.validator.htmlparser" % "htmlparser" % "1.2.1" intransitive(),
     "com.codecommit" %% "anti-xml" % "0.4-SNAPSHOT" from "http://jay.w3.org/~bertails/jar/anti-xml_2.10_20130110.jar",
-    "org.w3" % "validators" % "1.0-SNAPSHOT" from "http://jay.w3.org/~bertails/jar/validators-20130110.jar",
+    "org.w3" % "validators" % "1.0-SNAPSHOT" from "http://jay.w3.org/~bertails/jar/validators-20130328.jar",
     "com.yammer.metrics" % "metrics-core" % metricsVersion excludeAll(ExclusionRule(organization = "org.slf4j")),
     "com.yammer.metrics" % "metrics-graphite" % metricsVersion excludeAll(ExclusionRule(organization = "org.slf4j")),
     "org.reactivemongo" %% "play2-reactivemongo" % "0.9-SNAPSHOT" /*cross CrossVersion.full*/ excludeAll(ExclusionRule(organization = "io.netty"), ExclusionRule(organization = "play")),
