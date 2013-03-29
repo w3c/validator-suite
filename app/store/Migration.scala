bf0a7afb661ff193a728c61ec56ef963a1392fa9
Thomas Gambet <thomas.gambet@gmail.com>
2013-03-29 16:41:17 +0100

    removed migration code

--- app/store/Migration.scala
+++ app/store/Migration.scala
@@ -31,62 +31,8 @@ import org.w3.vs.store.Formats._
 
 object Migration {
 
-  val node = "localhost:27017"
-  val dbNameIn = "vs"
-  val dbNameOut = "vs-test"
-
   def main(args: Array[String]): Unit = {
-    migrationMarch28()
-  }
-
-  def migrationMarch28(): Unit = {
-
-    val driver = new reactivemongo.api.MongoDriver
-    val connection = driver.connection(Seq(node))
-
-    val dbIn =
-      connection(dbNameIn)
-
-    val dbOut =
-      connection(dbNameOut)
-
-    dbOut.drop()
-
-    val usersIn = dbIn("users")
-
-    val usersOut: BSONCollection = dbOut("users")
-
-    val jobsIn = dbIn("jobs")
-
-    val jobsOut = dbOut("jobs")
-
-    // copy users
-    val users: List[JsValue] = usersIn.find(Json.obj()).cursor[JsValue].toList.getOrFail()
-
-    users foreach { user =>
-      println(user \ "email")
-      usersOut.insert(user).getOrFail()
-    }
-
-    // copy jobs
-    val jobs: List[JsValue] = jobsIn.find(Json.obj()).cursor[JsValue].toList.getOrFail()
-
-    jobs foreach { json =>
-      val job = Job(
-        id = (json \ "_id").as[JobId],
-        name = (json \ "name").as[String],
-        createdOn = (json \ "createdOn").as[DateTime],
-	strategy = (json \ "strategy").as[Strategy],
-	creatorId = (json \ "creator").as[UserId],
-	status = NeverStarted,
-	latestDone = None)
-//      println(json)
-      println(job.name)
-      jobsOut.insert(toJson(job)).getOrFail()
-    }
-
-    println("done with migrationMarch28")
-
+    println("no migration configured")
   }
 
 }
