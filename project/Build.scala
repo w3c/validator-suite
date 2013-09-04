db51217e66e1c2ccaf8304e877ce935e530deec8
Thomas Gambet <thomas.gambet@gmail.com>
2013-09-04 18:02:29 +0200

    fixed Build.scala imports

--- project/Build.scala
+++ project/Build.scala
@@ -1,14 +1,15 @@
 import sbt._
 import Keys._
-import play.Project.{ fork => _ }
+import play.Project.{fork => _, _}
 import net.tgambet.requirejs.RequireJsPlugin._
+
 //import org.ensime.sbt.Plugin.Settings.ensimeConfig
 //import org.ensime.sbt.util.SExp._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "validator-suite"
-  val appVersion      = "0.2"
+  val appName = "validator-suite"
+  val appVersion = "0.2"
 
   val akkaVersion = "2.1.0"
   val scalazVersion = "7.0.0"
@@ -21,8 +22,8 @@ object ApplicationBuild extends Build {
     "org.apache.commons" % "commons-lang3" % "3.1" intransitive(), // For StringUtils escaping functions
     "com.codecommit" %% "anti-xml" % "0.4-SNAPSHOT" from "http://jay.w3.org/~bertails/jar/anti-xml_2.10_20130110.jar",
     "org.w3" % "validators" % "1.0-SNAPSHOT" from "http://jay.w3.org/~bertails/jar/validators-20130328-2.jar",
-    "com.codahale.metrics" % "metrics-core" % metricsVersion excludeAll(ExclusionRule(organization = "org.slf4j")),
-    "com.codahale.metrics" % "metrics-graphite" % metricsVersion excludeAll(ExclusionRule(organization = "org.slf4j")),
+    "com.codahale.metrics" % "metrics-core" % metricsVersion excludeAll (ExclusionRule(organization = "org.slf4j")),
+    "com.codahale.metrics" % "metrics-graphite" % metricsVersion excludeAll (ExclusionRule(organization = "org.slf4j")),
     "org.reactivemongo" %% "play2-reactivemongo" % "0.9" /*cross CrossVersion.full*/ excludeAll(ExclusionRule(organization = "io.netty"), ExclusionRule(organization = "play")),
     "org.scalaz" %% "scalaz-core" % scalazVersion,
     "org.mindrot" % "jbcrypt" % "0.3m",
@@ -33,18 +34,20 @@ object ApplicationBuild extends Build {
     "org.scalatest" %% "scalatest" % scalatestVersion
   )
 
-//  val assertorApi = Project("assertor-api", file("assertor-api"))
+  //  val assertorApi = Project("assertor-api", file("assertor-api"))
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     scalaVersion := "2.10.2",
 
     // activates full stacktrace and durations
     testOptions in Test := Nil,
-    testOptions in Test += Tests.Argument("""-oDF"""),
+    testOptions in Test += Tests.Argument( """-oDF"""),
 
     scalacOptions ++= Seq("-deprecation", "-unchecked", /* "-optimize",*/ "-feature", "-language:implicitConversions,higherKinds,reflectiveCalls"),
-    scalacOptions in (Compile, doc) ++= Opts.doc.title("W3C Validator Suite"),
-    scalacOptions in (Compile, doc) <++= baseDirectory map { bd => Seq("-sourcepath", bd.getAbsolutePath, "-doc-source-url", "https://github.com/w3c/validator-suite/tree/master€{FILE_PATH}.scala") },
+    scalacOptions in(Compile, doc) ++= Opts.doc.title("W3C Validator Suite"),
+    scalacOptions in(Compile, doc) <++= baseDirectory map {
+      bd => Seq("-sourcepath", bd.getAbsolutePath, "-doc-source-url", "https://github.com/w3c/validator-suite/tree/master€{FILE_PATH}.scala")
+    },
 
     routesImport += "org.w3.vs.controllers._",
     routesImport += "org.w3.vs.model._",
@@ -62,24 +65,24 @@ object ApplicationBuild extends Build {
 
     logLevel := Level.Debug,
     resolvers += "Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
-//    resolvers += "sgodbillon" at "https://bitbucket.org/sgodbillon/repository/raw/master/snapshots/"
-//    resolvers += "jay-bertails" at "http://jay.w3.org/~bertails/ivy/"
-//    resolvers += "repo.codahale.com" at "http://repo.codahale.com",
-//    resolvers += "apache-repo-releases" at "http://repository.apache.org/content/repositories/releases/"
-//    ensimeConfig := sexp(
-//      key(":compiler-args"), sexp("-Ywarn-dead-code", "-Ywarn-shadowing"),
-//      key(":formatting-prefs"), sexp(
-//        key(":rewriteArrowSymbols"), false,
-//        key(":doubleIndentClassDeclaration"), false
-//      )
-//    )
+    //    resolvers += "sgodbillon" at "https://bitbucket.org/sgodbillon/repository/raw/master/snapshots/"
+    //    resolvers += "jay-bertails" at "http://jay.w3.org/~bertails/ivy/"
+    //    resolvers += "repo.codahale.com" at "http://repo.codahale.com",
+    //    resolvers += "apache-repo-releases" at "http://repository.apache.org/content/repositories/releases/"
+    //    ensimeConfig := sexp(
+    //      key(":compiler-args"), sexp("-Ywarn-dead-code", "-Ywarn-shadowing"),
+    //      key(":formatting-prefs"), sexp(
+    //        key(":rewriteArrowSymbols"), false,
+    //        key(":doubleIndentClassDeclaration"), false
+    //      )
+    //    )
   ).settings(requireBaseSettings: _*)
-   .settings(
-    RequireJS.baseDir   <<= (baseDirectory)(_ / "project" / "target"),
+    .settings(
+    RequireJS.baseDir <<= (baseDirectory)(_ / "project" / "target"),
     RequireJS.sourceDir <<= (baseDirectory)(_ / "app" / "assets" / "js"),
     RequireJS.targetDir <<= (resourceManaged in Compile)(_ / "public" / "js"),
     RequireJS.cacheFile <<= cacheDirectory(_ / "requirejs"),
     javascriptEntryPoints := PathFinder.empty
-   )
+  )
 
 }
