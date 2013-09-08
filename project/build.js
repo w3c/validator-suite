ebfa0b7b51afbd5cf3b1e7a13eb5dea76e1006ea
Thomas Gambet <thomas.gambet@gmail.com>
2013-09-08 22:05:46 +0200

    new screenshot + scripts minification

--- project/build.js
+++ project/build.js
@@ -3,22 +3,23 @@
     baseUrl: "./",
     //mainConfigFile: '../js/config.js',
     preserveLicenseComments: false,
+    optimize: "uglify",
     wrap: {
         startFile: ["../js/licences.txt"],
         endFile: []
     },
     paths: {
-        'libs/query': "empty:"
+        //'libs/query': "libs/jquery" //"empty:"
     },
     shim: {
         'libs/underscore': { exports: '_' },
         'libs/modernizr':  { exports: 'Modernizr' },
         'libs/backbone': {
-            deps: ['libs/underscore', 'libs/query'],
+            deps: ['libs/underscore', 'libs/jquery'],
             exports: 'Backbone'
         },
         'libs/foundation': {
-            deps: ["libs/modernizr", "libs/query"],
+            deps: ["libs/modernizr", "libs/jquery"],
             exports: "Foundation"
         },
         'libs/foundation.reveal': ['libs/foundation'],
@@ -26,8 +27,11 @@
     },
     modules: [
         {
-            name: "libs/require",
-            include: ['config' ]
+            name: 'libs/jquery'
+        },
+        {
+            name: "libs/require"
+            //include: ['config' ]
         },
         {   name: "libs/backbone"   },
         {
@@ -36,10 +40,10 @@
         },
         {
             name: "main",
-            exclude: ["libs/backbone", "model/vs"]
+            exclude: ["libs/backbone", "model/vs"],
         },
         {
-            name: "front"
+            name: "front",
         }
     ]
 })
