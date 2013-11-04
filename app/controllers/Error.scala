99a495cb38b3bff3f69571965b9784421c0f6b08
Vivien Lacourba <vivien@w3.org>
2013-11-04 13:33:38 +0100

    New 403 Forbidden error page

--- app/controllers/Error.scala
+++ app/controllers/Error.scala
@@ -7,6 +7,7 @@ object Error extends VSController {
   val logger = play.Logger.of("controllers.Error")
 
   def error400 = Action { implicit req => Ok(views.html.error._400()) }
+  def error403 = Action { implicit req => Ok(views.html.error._403()) }
   def error404 = Action { implicit req => Ok(views.html.error._404()) }
   def error50x = Action { implicit req => Ok(views.html.error._50x()) }
   def error500 = Action { implicit req => Ok(views.html.error._500()) }
